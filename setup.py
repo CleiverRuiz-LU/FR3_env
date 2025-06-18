2d2b08074c1c8c9b34b00db37bded2deeec8eeab
Juan Rojas <rojas70@gmail.com>
2025-06-17 21:56:55 -0500

    6/17, started from stratch

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="panda_mujoco_gym",
+    name="panda-mujoco-gym",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "gymnasium>=0.29.1",
-        "gymnasium-robotics>=1.2.2",
-        "mujoco>=2.3.3",
-        "numpy>=1.23.5",
+        "mujoco==2.3.3",
+        "gymnasium==0.29.1", 
+        "gymnasium-robotics==1.2.2",
+        "stable-baselines3==2.2.1"
     ],
 ) 
